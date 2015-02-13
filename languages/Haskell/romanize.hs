86c9fd6835f1b684b778d726f64954c945466f66
David Eisinger <david.eisinger@gmail.com>
2015-02-13 15:34:11 -0500

    Haskell: refactor & comment romanize

--- languages/Haskell/romanize.hs
+++ languages/Haskell/romanize.hs
@@ -1,5 +1,5 @@
 import System.Environment(getArgs)
-import Data.List(maximumBy, find)
+import Data.List(find)
 
 (|>) :: a -> (a -> b) -> b
 (|>) = flip ($)
@@ -13,48 +13,69 @@ conversions =  [('I', 1),
                 ('D', 500),
                 ('M', 1000)]
 
+-- | Given a Roman character, get its Arabic value
 valueOf :: Char -> Int
 valueOf roman = case lookup roman conversions of
   Just i  -> i
   Nothing -> error "Invalid Roman numeral"
 
+-- | Given an Arabic number, return its Roman/Arabic pair or nothing
+possibleRoman :: Int -> Maybe (Char, Int)
+possibleRoman number = find ((number ==) . snd) conversions
+
+-- | Given an Arabic number, return its Roman character
 romanOf :: Int -> Char
-romanOf number = case find (\(_, a) -> a == number) conversions of
+romanOf number = case possibleRoman number of
   Just i  -> fst i
-  Nothing -> error "Something has gone awry in romanOf"
+  Nothing -> error "Invalid Arabic numeral"
 
+-- | Return the highest Roman/Arabic pair less than or equal to the number
 highestContained :: Int -> (Char, Int)
-highestContained arabic = conversions
-                            |> filter (\(_, a) -> a <= arabic)
-                            |> maximumBy (\(_, a1) (_, a2) -> compare a1 a2)
+highestContained arabic = conversions |> filter ((arabic >=) . snd) |> last
 
-fourth :: [Char] -> Char -> Bool
-fourth roman ch
+-- | Are the first four Roman characters all the same?
+firstFourCombinable :: [Char] -> Bool
+firstFourCombinable roman
+  | (length roman < 4) = False
+  | otherwise = all (head roman ==) (take 4 roman)
+
+-- | Combine the first four characters
+-- | E.g. IIII -> IV
+combineFirstFour :: [Char] -> [Char]
+combineFirstFour roman = big_roman : small_roman : (drop 4 roman)
+  where
+    small_roman = head roman
+    big_roman   = romanOf (5 * valueOf small_roman)
+
+-- | Are the first and third Roman characters the same and is there a Roman
+-- | character for their double?
+firstAndThirdCombinable :: [Char] -> Bool
+firstAndThirdCombinable roman
   | (length roman < 3) = False
-  | (all (ch ==) (take 3 roman)) = True
-  | otherwise = False
+  | otherwise = case roman |> head |> valueOf |> (2 *) |> possibleRoman of
+    Just _  -> head roman == roman !! 2
+    Nothing -> False
 
-compactTwo :: [Char] -> [Char]
-compactTwo roman
-  | (length roman < 3) = roman
-  | (head roman == roman !! 2) = big_roman : small_roman : (drop 3 roman)
-  | otherwise = roman
-    where
-      small_roman = roman !! 1
-      big_roman   = romanOf (2 * valueOf (head roman))
+-- | Combine the first and third characters
+-- | E.g. VIV -> IX
+combineFirstAndThird :: [Char] -> [Char]
+combineFirstAndThird roman =  big_roman : small_roman : (drop 3 roman)
+  where
+    small_roman = roman !! 1
+    big_roman   = romanOf (2 * valueOf (head roman))
 
-compactFour :: [Char] -> [Char]
-compactFour roman = compactTwo (big_roman : small_roman : (drop 4 roman))
-    where
-      small_roman = head roman
-      big_roman   = romanOf (5 * valueOf small_roman)
-
+-- | Given a string of Roman characters and an integer, recursively:
+-- | 1. Combine first four characters if possible
+-- | 2. Combine first and third characters if possible
+-- | 3. Reverse the Roman string and return it if Arabic number is zero
+-- | 4. Find the largest Roman character that fits wthin the Arabic
+-- |    number, add it to the string, subtract its value from the number
 romanize :: [Char] -> Int -> [Char]
 romanize roman arabic
+  | firstFourCombinable roman = romanize (combineFirstFour roman) arabic
+  | firstAndThirdCombinable roman = romanize (combineFirstAndThird roman) arabic
   | (arabic == 0) = reverse roman
-  | (fourth roman r) = romanize (compactFour (r:roman)) (arabic - a)
-  | otherwise = romanize (r:roman) (arabic - a)
-    where (r, a) = highestContained arabic
+  | otherwise = romanize (r:roman) (arabic - a) where (r, a) = highestContained arabic
 
 main :: IO ()
 main = do
